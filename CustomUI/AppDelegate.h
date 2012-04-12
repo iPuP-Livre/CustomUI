9bde735af02ed1afdafdcb4cae16da565df1db50
Jérémy Lagrue <jeremy.lagrue@orange.fr>
2012-04-12 09:08:58 +0200

    Initial Commit

---
+++ CustomUI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomUI
+//
+//  Created by Jérémy Lagrue on 12/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
